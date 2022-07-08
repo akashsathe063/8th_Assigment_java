347b9188bd04f2c7a3b52ff1451e29af5e9564ba
akashsathe063 <akashsathe063@gmail.com>
2022-07-08 21:27:39 +0530

    Employee wage by using class and method

--- src/com/blz/employee_wage/EmployeeWage.java
+++ src/com/blz/employee_wage/EmployeeWage.java
@@ -10,23 +10,28 @@ public class EmployeeWage {
     static int part_time_hours = 4;
     static int wage = 0;
     static int total_wage = 0;
-    static  int total_hours = 0;
+    static int total_hours = 0;
 
     public static void main(String[] args) {
         System.out.println("Well come to Employee Wage computation");
+        employee_Wage();
+    }
+
+    public static void employee_Wage() {
         int day = 0;
-        while(total_hours < max_working_hours &&  day<= working_day_per_month ) {
+        while (total_hours < max_working_hours && day <= working_day_per_month) {
             int checkRandom = (int) Math.floor(Math.random() * 10) % 3;
             switch (checkRandom) {
-                case 1:if(total_hours + full_day_hours < max_working_hours) {
-                    System.out.println("Employee is present and day of month: " + day++);
-                    wage = wage_per_hour * full_day_hours;
-                    total_wage = total_wage + wage;
-                    total_hours = total_hours + full_day_hours;
-                }
+                case 1:
+                    if (total_hours + full_day_hours < max_working_hours) {
+                        System.out.println("Employee is present and day of month: " + day++);
+                        wage = wage_per_hour * full_day_hours;
+                        total_wage = total_wage + wage;
+                        total_hours = total_hours + full_day_hours;
+                    }
                     break;
                 case 2:
-                    if(total_hours + part_time_hours < max_working_hours) {
+                    if (total_hours + part_time_hours < max_working_hours) {
                         System.out.println("Employee is part time present and day of month: " + day++);
                         wage = wage_per_hour * part_time_hours;
                         total_wage = total_wage + wage;
@@ -40,7 +45,8 @@ public class EmployeeWage {
             }
 
         }
-        System.out.println("Total Hours: "+total_hours);
+        System.out.println("Total Hours: " + total_hours);
         System.out.println("Month Employee Wage: " + total_wage);
     }
 }
+
