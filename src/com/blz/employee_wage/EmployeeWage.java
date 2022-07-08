d17c3980059b4bfc6b4728e351cca5effa9faf09
akashsathe063 <akashsathe063@gmail.com>
2022-07-08 19:13:21 +0530

    Well come to Employee Wage

---
+++ src/com/blz/employee_wage/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.blz.employee_wage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Well come to Employee Wage computation");
+    }
+}
